782db948222ffb1b914fb793c5e30d310381221e
kosuke-w <s1lv3rt1psh4rk@gmail.com>
2021-07-04 07:35:09 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7a9abe32351954d950d52b27a1885f32c0d837dddec4e5001168e78f252088a2be51c0e35c5fc39562e5370bf0d1ac92bd75d4e8b2970a181283813362b7e21f'
